583c38bbc7e5255eec636896ee9d79e4e2550baa
FZhu <hzhu225@gmail.com>
2018-12-19 15:52:05 +1100

    Add Url part

--- app/src/main/java/com/cms/implicitintents/MainActivity.java
+++ app/src/main/java/com/cms/implicitintents/MainActivity.java
@@ -1,21 +1,39 @@
 package com.cms.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    private EditText urlEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        urlEditText = findViewById(R.id.url_editText);
     }
 
     public void openWebsite(View view)
     {
+        String urlContent = urlEditText.getText().toString();
+        Uri uri = Uri.parse(urlContent);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if(intent.resolveActivity(getPackageManager())!= null)
+        {
+           startActivity(intent);
+        }
+        else
+        {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
     }
 
     public void openLocation(View view)
